3baf6a870e012d188d8a3c5311a92667a2b69349
Wise Ilya <johndoe@example.com>
2019-01-21 17:43:32 +0200

    21.01.19

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,19 @@
-    var LInH = document.getElementById('LInH');
+
+
+import React, { Component } from 'react';
+import React from 'react';
+
+
+
+    ReactDOM.render(
+        <div>
+            <h1>Hello friend</h1>
+        </div>,
+        document.getElementById('app')
+    );
+
+
+var LInH = document.getElementById('LInH');
 
     LInH.addEventListener('click', function () {
         alert('Work this button');
