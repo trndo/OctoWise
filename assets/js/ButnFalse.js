2363cad20a96c35feb13da8f9ce0dab30f4ad5d0
Wise Ilya <johndoe@example.com>
2019-03-04 17:33:39 +0200

    NewFReactCompop

---
+++ assets/js/ButnFalse.js
@@ -0,0 +1,26 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
+
+import '../css/app.css'
+
+    class TurnF extends React.Comment{
+        constructor(props,stps) {
+            super(props);
+            var stps = new Stps();
+        }
+
+
+
+
+
+        render() {
+            return(
+                Stps()
+            )
+        }
+    }
+
+
+
+    export default TurnF;
