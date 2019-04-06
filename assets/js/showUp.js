8df2ab9d63c4d53305547f57adb834bce2398fae
trndo <vetal1199@gmail.com>
2019-04-06 14:22:41 +0300

    Added js scripts in webpack

---
+++ assets/js/showUp.js
@@ -0,0 +1,8 @@
+function showP(){
+    let list =  document.getElementsByClassName('willHide');
+    for (let i = 0; i < list.length; i++) {
+        list[i].style.display = 'block';
+    }
+    document.getElementById('back').removeAttribute('class');
+    document.getElementById('button_show').style.display = 'none';
+}
